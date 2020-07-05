5cd6367b65d08c7858f97c26daad6b5172fd8413
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-05 16:00:14 -0300

    include config_apache.py

--- config_apache.py
+++ config_apache.py
@@ -1,19 +1,79 @@
-import sys, getopt
+import sys, getopt, os
+
+
+def save_file(filename, content):
+    file = open(filename, "w")
+    file.write(content)
+    file.close()
+
+
+def read_file(filename):
+    import codecs
+    file = codecs.open(filename, "r", "utf-8")
+    content = file.read()
+    file.close()
+    return content
+
+
+APACHE_CONFIG = """
+    Alias /static /home/static
+    <Directory /home/static>
+        Require all granted
+    </Directory>
+
+    Alias /static /home/media
+    <Directory /home/media>
+        Require all granted
+    </Directory>
+
+    <Directory /home/%(project)s>
+        <Files wsgi.py>
+            Require all granted
+        </Files>
+    </Directory>
+
+    WSGIDaemonProcess %(project)s python-home=/home/venv_%(project)s python-path=/home/%(project)s
+    WSGIProcessGroup %(project)s
+    WSGIScriptAlias / %(wsgi_path)s
+
+</VirtualHost>
+"""
+
 
 def main(argv):
-   project = ''
-   try:
-      opts, args = getopt.getopt(argv,"hp:",["project=",])
-   except getopt.GetoptError:
-      print('config_apache.py -p <project>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('config_apache.py -p <project>')
-         sys.exit()
-      elif opt in ("-p", "--project"):
-         project = arg
-   print('Project is %s' % project)
+    project = ''
+    try:
+        opts, args = getopt.getopt(argv,"hp:",["project=",])
+    except getopt.GetoptError:
+        print('config_apache.py -p <project>')
+        sys.exit(2)
+    for opt, arg in opts:
+    if opt == '-h':
+        print('config_apache.py -p <project>')
+        sys.exit()
+    elif opt in ("-p", "--project"):
+        project = arg
+    print('Project is %s' % project)
+    filename = '/etc/apache2/sites-available/000-default.conf'
+    content = read_file(filename)
+    data = {}
+    data['project'] = project
+    apps = os.listdir('/home/%s/' % project)
+    data['wsgi_path'] = ''
+    for app in apps:
+        if os.path.isfile('/home/%s/%s/wsgi.py' % (project, app)):
+            data['wsgi_path'] = '/home/%s/%s/wsgi.py' % (project, app)
+    if data['wsgi_path']:
+        save_file(
+            filename, 
+            content.replace(
+                '</VirtualHost>', 
+                APACHE_CONFIG % data)
+            )
+        print('Apache configured!')
+    else:
+        print('Apache cannot be configured!')
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
--- install_python3_django_venv_ubuntu.sh
+++ install_python3_django_venv_ubuntu.sh
@@ -23,7 +23,7 @@ else
     exit
 fi
 
-cd /home
+
 sudo apt update
 sudo apt autoremove
 python3 -V
@@ -33,13 +33,16 @@ sudo apt-get install apache2 libapache2-mod-wsgi-py3
 sudo apt-get install python3-psycopg2 python-psycopg2-doc
 pip3 install wheel
 sudo apt-get install libpq-dev python-dev
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default_backup
+python3 config_apache.py -p $PROJECT
 
-
-python3 -m venv venv_$PROJECT
+python3 -m venv /home/venv_$PROJECT
 source /home/venv_$PROJECT/bin/activate
 rm -rf /home/$PROJECT
 git clone $GIT /home/$PROJECT
 pip3 install -r /home/$PROJECT/requirements.txt
+mkdir /home/static
+mkdir /home/media
 
 # Install PostgreSQL on Ubuntu 18.04 Server
 # https://www.howtoforge.com/how-to-install-postgresql-and-pgadmin4-on-ubuntu-1804-lts/
