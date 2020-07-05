515697e823101ad63b77d542068f14a21244b0b4
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-05 16:05:32 -0300

    include config_apache.py

--- install_python3_django_venv_ubuntu.sh
+++ install_python3_django_venv_ubuntu.sh
@@ -3,17 +3,6 @@
 export GIT="$1"
 export PROJECT="$2"
 
-echo "$GIT"
-echo "$PROJECT"
-
-#while [[ "$#" > 0 ]]; do
-#    case $1 in
-#        -g|--git) GIT="$2"; shift ;;
-#        -p|--project) PROJECT="$1";  shift;;
-#esac
-#    shift
-#done
-
 if [[ "$GIT" ]] 
 then
     echo "git: $GIT"
@@ -22,7 +11,6 @@ else
     echo "A variável não existe"
     exit
 fi
-
 
 sudo apt update
 sudo apt autoremove
@@ -33,9 +21,6 @@ sudo apt-get install apache2 libapache2-mod-wsgi-py3
 sudo apt-get install python3-psycopg2 python-psycopg2-doc
 pip3 install wheel
 sudo apt-get install libpq-dev python-dev
-cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default_backup
-python3 config_apache.py -p $PROJECT
-
 python3 -m venv /home/venv_$PROJECT
 source /home/venv_$PROJECT/bin/activate
 rm -rf /home/$PROJECT
@@ -43,13 +28,20 @@ git clone $GIT /home/$PROJECT
 pip3 install -r /home/$PROJECT/requirements.txt
 mkdir /home/static
 mkdir /home/media
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default_backup
+sudo chown :www-data /home/$PROJECT
+sudo ufw delete allow 8000
+sudo ufw allow 'Apache Full'
+sudo apache2ctl configtest
+sudo systemctl restart apache2
+python3 config_apache.py -p $PROJECT
 
 # Install PostgreSQL on Ubuntu 18.04 Server
 # https://www.howtoforge.com/how-to-install-postgresql-and-pgadmin4-on-ubuntu-1804-lts/
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-sudo apt update
-sudo apt -y install postgresql postgresql-contrib
-sudo apt install pgadmin4 pgadmin4-apache2 -y
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+# sudo apt update
+# sudo apt -y install postgresql postgresql-contrib
+# sudo apt install pgadmin4 pgadmin4-apache2 -y
 
