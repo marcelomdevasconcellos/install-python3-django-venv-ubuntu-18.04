4eb94225fd01380de3ebf4a953a399e3545aa254
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-05 15:33:30 -0300

    include apache config

--- config_apache.py
+++ config_apache.py
@@ -6,14 +6,14 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hp:",["project=",])
    except getopt.GetoptError:
-      print 'config_apache.py -p <project>'
+      print('config_apache.py -p <project>')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'config_apache.py -p <project>'
+         print('config_apache.py -p <project>')
          sys.exit()
       elif opt in ("-p", "--project"):
-   print 'Project is "', project
+   print('Project is {}' % project)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
