5b423275698a8b2194eadb0fc3f9ea0e08632f70
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-06 08:25:59 -0300

    update

--- install_python3_django_venv_ubuntu.sh
+++ install_python3_django_venv_ubuntu.sh
@@ -17,18 +17,18 @@ sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo apt autoremove
 python3 -V
-sudo apt install python3-pip
-sudo apt install python3-venv
-sudo apt-get install apache2 libapache2-mod-wsgi-py3
+sudo apt -y install python3-pip python3-venv
+sudo apt-get install apache2 libapache2-mod-wsgi-py3 libpq-dev python-dev
 pip3 install wheel
-sudo apt-get install libpq-dev python-dev
 python3 -m venv /home/venv_$PROJECT
+
 source /home/venv_$PROJECT/bin/activate
 pip3 install psycopg2-binary
 rm -rf /home/$PROJECT
 git clone $GIT /home/$PROJECT
 pip3 install -r /home/$PROJECT/requirements.txt
 deactivate
+
 mkdir /home/static
 mkdir /home/media
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default_backup
@@ -36,16 +36,13 @@ sudo chown www-data:www-data /home/$PROJECT
 sudo chown www-data:www-data /home/static
 sudo chown www-data:www-data /home/media
 
-# Install PostgreSQL on Ubuntu 18.04 Server
-# https://www.howtoforge.com/how-to-install-postgresql-and-pgadmin4-on-ubuntu-1804-lts/
-
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt update
 sudo apt -y install postgresql postgresql-contrib
 
 sudo apt update
-sudo apt install libgmp3-dev libpq-dev libapache2-mod-wsgi-py3
+sudo apt -y install libgmp3-dev libpq-dev libapache2-mod-wsgi-py3
 sudo mkdir -p /var/lib/pgadmin4/sessions
 sudo mkdir /var/lib/pgadmin4/storage
 sudo mkdir /var/log/pgadmin4
@@ -69,8 +66,8 @@ sudo chown -R www-data:www-data /var/lib/pgadmin4/
 sudo chown -R www-data:www-data /var/log/pgadmin4/
 sudo /etc/init.d/postgresql start
 pg_ctlcluster 12 main start
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
-
+sudo -u postgres psql -c "CREATE ROLE $PROJECT;"
+sudo -u postgres psql -c "ALTER USER $PROJECT PASSWORD '$PROJECT';"
 python3 config_apache.py -p $PROJECT
 sudo ufw delete allow 8000
 sudo ufw allow 'Apache Full'
