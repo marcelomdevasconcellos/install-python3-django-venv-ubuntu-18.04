564e59f8d3834bc1e549b0665ac783fc5952359b
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-06 20:55:39 -0300

    include wkhtmltopdf

--- install_python3_django_venv_ubuntu.sh
+++ install_python3_django_venv_ubuntu.sh
@@ -75,3 +75,7 @@ sudo ufw allow 'Apache Full'
 sudo apache2ctl configtest
 sudo systemctl restart apache2
 sudo dpkg-reconfigure locales
+
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
+sudo apt install -f
